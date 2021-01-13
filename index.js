852e787b790f299d3d529edf7eab3f0b5f9fe935
DenisLorand <73839528+DenisLorand@users.noreply.github.com>
2021-01-13 10:21:17 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+console.log ('hello');
+
+// Fetch => mdn
+//Récupérer 1 pokemon
+//GET
+//Adresse => url > endpoint
+
+//Pokeapi
+
+//Injester un pokemenon dans la balise #pokemon
+
+//JSON.parse (response)
