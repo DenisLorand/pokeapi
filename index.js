a8e1256a5cfa155f445082a2d300fa5693e1bc6a
DenisLorand <denis.lorand@yahoo.com>
2021-01-13 10:51:54 +0100

    update

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 console.log ('hello');
 
+let render = document.getElementById('pokemon');
+
+fetch('https://pokeapi.co/api/v2/pokemon/pikachu')
+.then(response => response.json())
+.then(data => {
+
+let img = createElement('img');
+img.src = data.sprites.front_shiny;
+
+let p = document.createElement('p');
+p.innerHTML = data.name;
+
+render.appendChild(img);
+render.appendChild(p);
+
+});
+
 // Fetch => mdn
 //Récupérer 1 pokemon
 //GET
